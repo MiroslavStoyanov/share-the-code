2161c9d8c4e0d0884be65a90bea33c58ab28a30b
Miroslav Stoyanov <miroslav.stoyanov@musala.com>
2021-02-03 18:06:03 +0200

    Added some configuration files and startup processes for the server

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -5,10 +5,13 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
+    ecmaVersion: 2020
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "quotes": "warn",
+    "eqeqeq": "warn"
   }
 };
--- server/app.js
+++ server/app.js
@@ -1,11 +1,36 @@
-var express = require("express");
-var app = express();
-var port = 3000;
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const MONGODB_URL = process.env.MONGODB_URL;
 
-app.get("/", (req, res) => {
- res.send("Hello World");
+mongoose
+    .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        if(process.env.NODE_ENV === "development") {
+            console.log("Connected to %s", MONGODB_URL);
+            console.log("App is running ... \n");
+            console.log("Press CTRL + C to stop the process. \n");
+    }})
+    .catch(err => {
+        console.error("App starting error:", err.message);
+        process.exit(1);
+    });
+
+const db = mongoose.connection;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+const port = process.env.NODE_ENV === "production" ? (process.env.PORT || 80) : 3000;
+app.listen(port, () => {
+	console.log("Server listening on port " + port);
 });
 
-app.listen(port, () => {
- console.log("Server listening on port " + port);
+app.all("*", function(req, res) {
+	throw new Error("Page not found.");
 });
